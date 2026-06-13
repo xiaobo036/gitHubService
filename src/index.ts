44c67c297843157f41006ced589177cefb18da0d
xiaobo036 <2572284725@qq.com>
2026-06-14 07:51:50 +0800

    feat: 继续完善GithubApi Server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 import { GithubService } from "./githubService.ts";
 import { Repo } from "./models/Repo.ts";
 import { User } from "./models/User.ts";
+import type { Request, Response } from "express";
+import express from "express";
+
+const app = express();
+
+const port = 3000;
+
+app.get("/user/:userName", (req: Request, res: Response) => {
+  const userName = req.params.userName;
+  if(!userName) {
+    res.send("Please provide a valid username");
+  }
+  console.log(userName);
+  const user = getUserInfo(userName as User["name"]);
+  console.log(user);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
 
 const Api: GithubService = new GithubService();
 
@@ -10,14 +30,7 @@ const getUserInfo = async (userName: User["name"]) => {
   const res = await Api.getUser(userName);
   if (res?.status === 200) {
     const user = new User(res.data);
-    const repoRes = await getRepos(user.name as string);
-    user.repos = repoRes?.data.map((rep: Repo) => new Repo(repoRes?.data));
-    console.log(user);
+    const repoRes = await Api.getRepos(userName);
+    user.repos = repoRes?.data.map((repo: Repo) => new Repo(repo));
   }
 };
-
-const getRepos = async (userName: User["name"]) => {
-  return await Api.getRepos(userName);
-};
-
-getUserInfo("xiaobo036");
